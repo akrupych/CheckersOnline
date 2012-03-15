30fe58c87eb87c09d52380a960356dbdcd135a89
RomanDerenko <romanderenko90@gmail.com>
2012-03-15 14:31:52 +0200

    Update QTProject/cell.cpp

--- QTProject/cell.cpp
+++ QTProject/cell.cpp
@@ -12,3 +12,29 @@ Cell::Cell(unsigned char fcolor)
     figure = new Figure(fcolor);
     canMoveTo = false;
 }
+
+bool Cell::isEmpty() const
+{
+    if( this->figure == NULL )
+    {
+        return true;
+    }
+    return false;
+}
+
+Figure* Cell::CellFigure()
+{
+    return figure;
+}
+
+void Cell::beat()
+{
+    delete figure;
+    figure = NULL;
+}
+
+void Cell::setCanMove(bool arg)
+{
+    canMoveTo = arg;
+}
+
