59d2943541fe6d835a18d888ba6acf109777b3d5
RomanDerenko <romanderenko90@gmail.com>
2012-03-15 14:34:30 +0200

    Update QTProject/field.h

--- QTProject/field.h
+++ QTProject/field.h
@@ -2,11 +2,12 @@
 #define FIELD_H
 #define ROW_NUMBER 8
 #define COLUMN_NUMBER 4
+#include "cell.h"
 #include <QtGui>                // support QGraphicsScene
 
 class Field
 {
-    Figure* GameCells[ROW_NUMBER][COLUMN_NUMBER];
+    Cell* GameCells[ROW_NUMBER][COLUMN_NUMBER];
 public:
     bool isEmpty(unsigned char row, unsigned char column)const;
     void showField(QGraphicsScene* scene)const;
