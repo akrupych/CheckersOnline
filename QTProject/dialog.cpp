1c3be027b610fd74b3385136b87b147cbf723a31
RomanDerenko <romanderenko90@gmail.com>
2012-03-15 14:32:53 +0200

    Update QTProject/dialog.cpp

--- QTProject/dialog.cpp
+++ QTProject/dialog.cpp
@@ -16,39 +16,7 @@ Dialog::Dialog(QWidget *parent) :
 
     Field val;
 
-
     val.showField(scene);
-
-
-
-
-//    scene->addLine(0,0, scene->height(), scene->width() );
-
-
-
-//  scene->addText("Hello, world!");
-
-
-
-//  ui->graphicsView->setRenderHint(QPainter::Antialiasing);
-
-/*
-
-    QPen mypen = QPen(Qt::red);
-
-    QBrush myy = QBrush(Qt::green);
-
-    QLineF TopLine(scene->sceneRect().topLeft(), scene->sceneRect().topRight());
-    QLineF LeftLine(scene->sceneRect().topLeft(), scene->sceneRect().bottomLeft());
-    QLineF RightLine(scene->sceneRect().topRight(), scene->sceneRect().bottomRight());
-    QLineF BottomLine(scene->sceneRect().bottomLeft(), scene->sceneRect().bottomRight());
-
-    scene->addLine(TopLine, mypen);
-    scene->addLine(LeftLine, mypen);
-    scene->addLine(RightLine, mypen);
-    scene->addLine(BottomLine, mypen);
-*/
-
 }
 
 Dialog::~Dialog()
