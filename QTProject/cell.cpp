7f1a4b44d1e7d9eb4c14742ac81ed6e171b27582
RomanDerenko <romanderenko90@gmail.com>
2012-03-19 20:55:25 +0200

    Update QTProject/cell.cpp

--- QTProject/cell.cpp
+++ QTProject/cell.cpp
@@ -1,16 +1,31 @@
 #include "cell.h"
 
-Cell::Cell()
+Cell::Cell(unsigned char row_n, unsigned char column_n)
 {
-    figure = NULL;
+    if(row_n >= ROW_NUMBER || column_n >= COLUMN_NUMBER)
+    {
+        string exep = "ERROR! unexpected argument in Cell::Cell(unsigned char row_n, unsigned char column_n)\n";
+        throw exep;
+    }
+
+    row = row_n;
+    column = column_n;
     canMoveTo = false;
+    figure = NULL;
 }
 
-
-Cell::Cell(unsigned char fcolor)
+Cell::Cell(unsigned char fcolor, unsigned char row_n, unsigned char column_n)
 {
-    figure = new Figure(fcolor);
+    if(row_n >= ROW_NUMBER || column_n >= COLUMN_NUMBER)
+    {
+        string exep = "ERROR! unexpected argument Cell(unsigned char fcolor, unsigned char row_n, unsigned char column_n)\n";
+        throw exep;
+    }
+
+    row = row_n;
+    column = column_n;
     canMoveTo = false;
+    figure = new Figure(fcolor);
 }
 
 bool Cell::isEmpty() const
@@ -22,11 +37,6 @@ bool Cell::isEmpty() const
     return false;
 }
 
-Figure* Cell::CellFigure()
-{
-    return figure;
-}
-
 void Cell::beat()
 {
     delete figure;
@@ -36,5 +46,26 @@ void Cell::beat()
 void Cell::setCanMove(bool arg)
 {
     canMoveTo = arg;
+}
+
+unsigned char Cell::getRow()const
+{
+    return row;
+}
+
+unsigned char Cell::getColumn()const
+{
+    return column;
+}
+
+Figure Cell::getFigure() const
+{
+    if( figure == NULL )
+    {
+        string exep = "ERROR! getFigure function cannot return value from NULL pointer!\n";
+        throw exep;
+    }
+
+    return *figure;
 }
 
